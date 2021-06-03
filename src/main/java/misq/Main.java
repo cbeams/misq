a27c7ef11bf09df7dc8d8afb82f0c2a207dec260
Chris Beams <chris@beams.io>
2021-06-03 11:30:48 +0200

    Initialize Gradle build and project sources
    Using the following command with Gradle 7.0.2:
    
        gradle init --type java-application
    
    With the following modifications being made afterward:
    
     - Remove generated comments
     - Remove app submodule
     - Rename App => Main
     - Add .idea to .gitignore
    
    Notes:
    
    - Runs cleanly at the command line under JDK16
      - but has no JDK16-specific syntax or build resctrictions as yet
    - Imports into and runs cleanly within IDEA 2021.1.2
    - It is assumed the Main/MainTest files will be deleted

---
+++ src/main/java/misq/Main.java
@@ -0,0 +1,11 @@
+package misq;
+
+public class Main {
+    public String getGreeting() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Main().getGreeting());
+    }
+}
---
+++ src/test/java/misq/MainTest.java
@@ -0,0 +1,13 @@
+package misq;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    @Test
+    void appHasAGreeting() {
+        Main classUnderTest = new Main();
+        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+    }
+}
